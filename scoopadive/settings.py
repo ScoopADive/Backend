b4bb3bdc06356343f11805aaa238e9e139b9d6ac
junnie082 <junnie082@gmail.com>
2025-05-15 15:06:34 +0900

    fix: DB_HOST 자료형 변경

--- scoopadive/settings.py
+++ scoopadive/settings.py
@@ -193,7 +193,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'PORT': int(os.environ.get('DB_PORT')),
     }
 }
 
