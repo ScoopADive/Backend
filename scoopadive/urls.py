787791b2dbb7479ae7fd9f828be16cfd5966fb80
junnie082 <junnie082@gmail.com>
2025-05-15 18:27:33 +0900

    feat: DRF 스타일의 로그인, 로그아웃, 회원가입 구현

--- scoopadive/urls.py
+++ scoopadive/urls.py
@@ -56,12 +56,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path("logbooks/", include("logbook.urls")),
     path("my_user/", include("my_user.urls")),
-    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    path('api/auth/logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
-
-    # path('api/logout/', LogoutView.as_view(), name='auth_logout'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
