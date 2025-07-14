5b1930eeae37b416d1c8527b4e03e84e2e3f19e2
junnie082 <junnie082@gmail.com>
2025-07-14 17:44:26 +0900

    feat: 홈화면의 Top Level Member 구현

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from . import views
-from rest_framework import routers
+from .views import TopLevelMember
 
 
 urlpatterns = [
+    path('top_level_member', TopLevelMember.as_view(), name='top_level_member'),
 ]
 
--- logbook/serializers.py
+++ logbook/serializers.py
@@ -6,4 +6,8 @@ class LogbookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Logbook
         fields = '__all__'
+        read_only_fields = ['user']
 
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
