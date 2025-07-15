5cf0248764fe8a4a3a3056d1eb99de80a5490639
junnie082 <junnie082@gmail.com>
2025-07-15 18:03:58 +0900

    feat: Job API 개발

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import TopLevelMember
+from .views import TopLevelMembersList, JobViewSet
 
+router = DefaultRouter()
+router.register('jobs', JobViewSet, basename='jobs')
 
 urlpatterns = [
-    path('top_level_member', TopLevelMember.as_view(), name='top_level_member'),
+    path('top_level_members', TopLevelMembersList.as_view(), name='top_level_members'),
+    path('', include(router.urls)),
 ]
+
 
