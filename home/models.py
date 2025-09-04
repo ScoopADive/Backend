2580b0930ac2b6b0201091171eab48b85d6977cd
junnie082 <junnie082@gmail.com>
2025-09-04 20:46:01 +0900

    feat: Job 에 created_at 어트리뷰트 추가

--- home/models.py
+++ home/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from auths.models import User
-
+from django.utils import timezone  # 반드시 import 필요
 # Create your models here.
 class Job(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)  # 자동 생성
