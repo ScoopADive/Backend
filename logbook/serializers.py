1627bc312e360083c464e083ef76cb600f5c777e
junnie082 <junnie082@gmail.com>
2025-09-05 15:24:55 +0900

    fix: Equipment 입력 방식 변경

--- logbook/serializers.py
+++ logbook/serializers.py
@@ -8,10 +8,13 @@ User = get_user_model()
 class LogbookSerializer(serializers.ModelSerializer):
     liked_by_current_user = serializers.SerializerMethodField()
     likes_count = serializers.SerializerMethodField()
+
     buddy_input = serializers.CharField(write_only=True, required=False, allow_blank=True)
     buddy = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False, allow_null=True)
-    equipment_ids = serializers.ListField(
-        child=serializers.IntegerField(), write_only=True, required=False
+
+    equipment = serializers.ListField(
+        child=serializers.CharField(),
+        required=False
     )
 
     class Meta:
@@ -21,7 +24,7 @@ class LogbookSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         buddy_input = validated_data.pop('buddy_input', None)
-        equipment_ids = validated_data.pop('equipment_ids', [])
+        equipment_names = validated_data.pop("equipment", [])
 
         # Buddy 처리
         if buddy_input and buddy_input.startswith('@'):
@@ -39,22 +42,11 @@ class LogbookSerializer(serializers.ModelSerializer):
         # Logbook 생성
         logbook = Logbook.objects.create(**validated_data)
 
-        # Equipment 연결
-        if equipment_ids:
-            equipment_objs = Equipment.objects.filter(id__in=equipment_ids)
-            logbook.equipment.set(equipment_objs)
+        for name in equipment_names:
+            eq, _ = Equipment.objects.get_or_create(name=name)
+            logbook.equipment.add(eq)
 
         return logbook
-
-    def get_liked_by_current_user(self, obj):
-        request = self.context.get('request')
-        user = getattr(request, 'user', None)
-        if user and user.is_authenticated:
-            return obj.likes.filter(id=user.id).exists()
-        return False
-
-    def get_likes_count(self, obj):
-        return obj.likes.count()
 
 
 class LogbookLikeSerializer(serializers.ModelSerializer):
