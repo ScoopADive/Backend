084ce64a620bacd6c68f26495549f91e4372526d
junnie082 <junnie082@gmail.com>
2025-09-06 09:06:39 +0900

    Revert "fix: /mypage/profile 의 응답에 license 추가"
    This reverts commit dfec8e046f8c049e25e3b0d2811bd0634fe5965f.

--- mypage/serializers.py
+++ mypage/serializers.py
@@ -30,7 +30,7 @@ class UserDetailSerializer(serializers.ModelSerializer):
         model = User
         fields = [
             'id', 'profile_image', 'email', 'username', 'country', 'introduction',
-            'date_joined', 'bucketlists', 'own_logbooks', 'friends', 'license'
+            'date_joined', 'bucketlists', 'own_logbooks', 'friends'
         ]
         read_only_fields = ['date_joined', 'bucketlists', 'own_logbooks']
 
