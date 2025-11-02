a03e06d72a3ec493b3f960402f965f2aacace201
junnie082 <junnie082@gmail.com>
2025-11-02 14:54:51 +0900

    feat: settings 앱 생성

--- settings/urls.py
+++ settings/urls.py
@@ -1,12 +1,10 @@
-from django.urls import path
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
-from mypage.views import GetMyProfileView
 from settings.views import PreferencesViewSet
 
 router = DefaultRouter()
 router.register('preferences', PreferencesViewSet, basename='preferences')
 
 urlpatterns = [
-    path('profile/', PreferencesViewSet.as_view(), name='mypage_profile'),
+    path('', include(router.urls)),
 ]
