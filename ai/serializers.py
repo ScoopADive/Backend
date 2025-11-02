30d3fb6ce52b267ef85a6c99c26811fb7c7920d1
junnie082 <junnie082@gmail.com>
2025-11-02 17:02:08 +0900

    fix: SpotsRecommendationView 클래스 authentication 코드 수정

--- ai/serializers.py
+++ ai/serializers.py
@@ -6,3 +6,7 @@ class SpotSerializer(serializers.ModelSerializer):
     class Meta:
         model = Spot
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
