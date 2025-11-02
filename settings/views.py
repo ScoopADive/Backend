8b8cabf9429c6b6526dcefae0af27d3e1a49271b
junnie082 <junnie082@gmail.com>
2025-11-02 15:25:20 +0900

    fix: PreferencesViewSet 클래스 수정

--- settings/views.py
+++ settings/views.py
@@ -7,10 +7,10 @@ from settings.serializers import PreferencesSerializer
 
 # Create your views here.
 class PreferencesViewSet(viewsets.ModelViewSet):
-    queryset = Preferences.objects.all()
     serializer_class = PreferencesSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Preferences.objects.filter(user=self.request.user)
+        return Preferences.objects.filter(user=self.request.user).order_by('id')
+
 
