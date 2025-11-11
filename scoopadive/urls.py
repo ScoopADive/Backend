a2e0281b4b3ea16845fc9ad4fcce50792dfba989
junnie082 <junnie082@gmail.com>
2025-11-11 21:46:26 +0900

    fix: schema_view 에서 url https://scoopadive.com/api 로 변경

--- scoopadive/urls.py
+++ scoopadive/urls.py
@@ -44,7 +44,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
-    url="https://api.scoopadive.com/api"
+    url="https://scoopadive.com/api"
 )
 
 
