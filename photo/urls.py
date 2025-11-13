7b2d33d4cf0aa711b567aaf57fe8c5dc690afd5a
junnie082 <junnie082@gmail.com>
2025-11-13 21:45:29 +0900

    fix: photo router url 변경

--- photo/urls.py
+++ photo/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from photo.views import PhotoViewSet
 
 router = DefaultRouter()
-router.register('photo', PhotoViewSet)
+router.register('', PhotoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
